ad25d0b851b49c12a486d9602c3f7a0e8cbc4973
MarsChrom <mescamillam06@gmail.com>
2025-07-30 13:53:59 -0500

    Basic Follow Camera and Enemy
    Enemy just follows from anywhere no radius yet
    Camera follows player naturally

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public GameObject player; // Reference to the player GameObject
+    public float speed = 3f;
+
+
+    private float distance;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction = player.transform.position - transform.position;
+
+        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+    }
+}
