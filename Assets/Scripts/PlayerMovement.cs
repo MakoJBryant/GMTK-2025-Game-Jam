a9754877154618556ae4839c9c58903eee7a24b0
MarsChrom <mescamillam06@gmail.com>
2025-07-30 13:15:53 -0500

    Dodge and Sprint Added
    Shift for sprint
    Space for Dodge

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,23 +4,68 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public float moveSpeed = 5f;
+    public float sprintSpeed = 10f;
+    public float dodgeSpeed = 7.5f;
+    public float dodgeDuration = 0.5f;
+    public float dodgeCooldown = 1f;
 
     public Rigidbody2D rb;
 
     Vector2 movement;
+    Vector2 dodgeDir;
+
+    bool isDodging = false;
+    bool canDodge = true;
+    float dodgeTimer = 0f;
+    float cooldownTimer = 0f;
 
     // Update is called once per frame
     void Update()
     {
-        // Input
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        if (!isDodging)
+        {
+            // Input
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
+            movement.Normalize(); // Normalize to prevent faster diagonal movement
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && canDodge && movement != Vector2.zero)
+        {
+            isDodging = true;
+            canDodge = false;
+            dodgeDir = movement; // Store the direction of the dodge
+            dodgeTimer = dodgeDuration; // Set the end time for the dodge
+        }
+
+        if (!canDodge)
+        {
+            cooldownTimer -= Time.deltaTime;
+            if(cooldownTimer <= 0f)
+            {
+                canDodge = true; // Reset dodge ability after cooldown
+            }
+        }
     }
 
     // Fixed Timer sort of like DeltaTime
     private void FixedUpdate()
     {
-        // Movement
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        if (isDodging)
+        {
+            rb.MovePosition(rb.position + dodgeDir * dodgeSpeed * Time.fixedDeltaTime);
+            dodgeTimer -= Time.fixedDeltaTime;
+            if (dodgeTimer <= 0)
+            {
+                isDodging = false;
+                cooldownTimer = dodgeCooldown;
+            }
+        }
+        else
+        {
+            float currentSpeed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : moveSpeed;
+            rb.MovePosition(rb.position + movement * currentSpeed * Time.fixedDeltaTime);
+        }
+
     }
 }
